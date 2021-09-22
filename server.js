0ecc665ce6a41d6f040512d5fe015d3f5d6bf7ec
augusto <gutim160@gmail.com>
2021-09-22 05:38:03 -0300

    metodos get na pagina inicial firebase

---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const MongoClient  = require('mongodb').MongoClient
+
+
+const firebase = require('firebase/app');
+const database = require('firebase/database')
+
+
+function addQuotes(name, quote) {
+	var newSenhasKey =  database.push(database.child(database.ref(db), 'quotes/')).key;
+	database.set(database.ref(db, `quotes/${newSenhasKey}`), {
+		name: name,
+		quote: quote,
+	}).then(() => {
+		console.log('Adicionado com sucesso!', {name: name, quote: quote})
+	}).catch((error) => {
+		console.log('Nao adicionado')
+	});
+}
+
+function recuperarDados() {
+  const starC = database.ref(db, 'quotes/')
+  database.onValue(starC, snap => {
+    const data = snap.val()
+    console.log(data)
+  })
+}
+
+const firebaseConfig = {
+	apiKey: "AIzaSyB-1Oqa0rCXLZv1mJGM0W4D63U9HE1lJ4s",
+	authDomain: "bamboo-analyst-271911.firebaseapp.com",
+	databaseURL: "https://bamboo-analyst-271911.firebaseio.com",
+	projectId: "bamboo-analyst-271911",
+	storageBucket: "bamboo-analyst-271911.appspot.com",
+	messagingSenderId: "790844234696",
+	appId: "1:790844234696:web:f38ad2a1a34c794b9b7efd"
+};
+
+firebase.initializeApp(firebaseConfig);
+var db = database.getDatabase()
+
+
+const PORT = process.env.PORT || 8080
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+	let array = []
+	const starC = database.ref(db, 'quotes/')
+	let result = database.onValue(starC, snap => {
+		snap.forEach(child => {
+			const chave = child.key
+			const name = child.val().name
+			const quote = child.val().quote
+			let objt = new Object();
+			objt.chave = chave;
+			objt.name = name;
+			objt.quote = quote;
+			array.push(objt)
+
+		})
+	})	
+	res.render('index.ejs', {quotes: array})	
+})
+
+
+app.post('/quotes', (req, res) => {
+	var newSenhasKey =  database.push(database.child(database.ref(db), 'quotes/')).key;
+	database.set(database.ref(db, `quotes/${newSenhasKey}`), {
+		name: req.body.name,
+		quote: req.body.quote
+	}).then(() => {
+		let result = { name: req.body.name, quote: req.body.quote }
+		res.redirect('/', {quote: result})
+	})
+	.catch(err => console.log(err))	
+})
+
+app.listen(PORT, () => {
+	console.log(`Servidor Rodando: ${process.env.HOST}${PORT}`)
+})
